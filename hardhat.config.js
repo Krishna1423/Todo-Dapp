6a498ff00024502f55136bbc69f86f9f6ecb6862
Krishna A S <krishnasaye23@gmail.com>
2024-12-06 17:59:31 -0500

    Removed unwanted console log

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-ethers");
 require("dotenv").config({ path: "D:/hardhat/config.env" });
-console.log("Alchemy URL: ", process.env.ALCHEMY_SEPOLIA_URL);
-console.log("Metamask: ", process.env.METAMASK_ACCOUNT_PRIVATE_KEY);
 
 module.exports = {
   defaultNetwork: "sepolia",
